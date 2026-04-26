ac4bdbe5c0604b1245d4c529fbb707daa81f99f1
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-04-26 18:35:43 +0530

    added response code

--- backend/15-mar-express-basics/01-express-intro.js
+++ backend/15-mar-express-basics/01-express-intro.js
@@ -92,8 +92,125 @@ function block_1_basicServer() {
   });
 }
 
+function block_2_response() {
+  return new Promise((resolve) => {
+    const app = express();
+
+    app.get("/text", (req, res) => {
+      res.send("Hello, this is a plain text response!");
+    });
+
+    app.get("/json", (req, res) => {
+      res.json({
+        framework: "Express",
+        version: "6.0.0",
+      });
+    });
+
+    app.get("/not-found", (req, res) => {
+      res.status(404).json({
+        error: "page not found",
+      });
+    });
+
+    app.get("/health", (req, res) => {
+      res.sendStatus(200); // sets status code and sends response
+    });
+
+    app.get("/old-menu", (req, res) => {
+      // add entry in db to check how many users are still visiting old menu endpoint
+      res.redirect(301, "/menu"); // redirect to new menu endpoint
+    });
+
+    app.get("/xml", (req, res) => {
+      res.type("application/xml").send(`
+          <menu>
+            <item>Biriyani</item>
+            <item>Chicken</item>
+          </menu>`);
+    });
+
+    app.get("/custom-header", (req, res) => {
+      res.set("X-Powered-By", "Chai Aur Code");
+      res.set("X-Request-Id", "1234567890");
+      res.json({
+        message: "Custom headers set in the response",
+      });
+    });
+
+    app.get("/no-content", (req, res) => {
+      res.status(204).end(); // No content to send in response
+    });
+
+    const server = app.listen(0, async () => {
+      const port = server.address().port;
+      const base = `http://localhost:${port}`;
+
+      try {
+        // TODO:
+        const res = await fetch(`${base}/text`);
+        const textData = await res.text();
+        console.log("GET /text", textData);
+
+        console.log("+".repeat(40));
+
+        const jsonRes = await fetch(`${base}/json`);
+        const jsonData = await jsonRes.json();
+        console.log("GET /json", JSON.stringify(jsonData));
+
+        console.log("+".repeat(40));
+
+        const notFoundRes = await fetch(`${base}/not-found`);
+        const notFoundData = await notFoundRes.json();
+        console.log("GET /not-found", JSON.stringify(notFoundData));
+
+        console.log("+".repeat(40));
+
+        const healthRes = await fetch(`${base}/health`);
+        console.log("GET /health", healthRes.status);
+
+        console.log("+".repeat(40));
+
+        const oldMenuRes = await fetch(`${base}/old-menu`, {
+          redirect: "manual",
+        });
+        console.log(
+          "GET /old-menu",
+          oldMenuRes.status,
+          oldMenuRes.headers.get("location"),
+        );
+
+        console.log("+".repeat(40));
+
+        const xmlRes = await fetch(`${base}/xml`);
+        const xmlData = await xmlRes.text();
+        console.log("GET /xml", xmlData);
+
+        console.log("+".repeat(40));
+
+        const customHeaderRes = await fetch(`${base}/custom-header`);
+        const customHeaderData = await customHeaderRes.json();
+        console.log(
+          "GET /custom-header",
+          customHeaderRes.headers.get("X-Powered-By"),
+          customHeaderRes.headers.get("X-Request-Id"),
+          JSON.stringify(customHeaderData),
+        );
+
+        console.log("+".repeat(40));
+
+        const noContentRes = await fetch(`${base}/no-content`);
+        console.log("GET /no-content", noContentRes);
+      } catch (error) {
+        console.error("Error fetching menu:", error);
+      }
+    });
+  });
+}
+
 async function main() {
   await block_1_basicServer();
+  await block_2_response();
 }
 
 main();
