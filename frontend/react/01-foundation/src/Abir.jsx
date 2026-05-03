659985c53889967ab4e9116a3a8e2558245a7292
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-05-03 23:21:30 +0530

    basic styling and folder structure

---
+++ frontend/react/01-foundation/src/Abir.jsx
@@ -0,0 +1,16 @@
+import styles from "./Abir.module.css";
+
+function Abir() {
+  return (
+    <div
+      style={{
+        backgroundColor: "orange",
+        color: "white",
+      }}
+    >
+      <h1 className={styles.abh1}>Hello from Abir</h1>
+    </div>
+  );
+}
+
+export default Abir;
