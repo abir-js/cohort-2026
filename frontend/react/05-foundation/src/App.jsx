6a0a293753d602b61c36810c9dfae0e10acffa78
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-05-05 10:22:28 +0530

    implemented abort controller

--- frontend/react/05-foundation/src/App.jsx
+++ frontend/react/05-foundation/src/App.jsx
@@ -1,18 +1,51 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [status, setStatus] = useState("idle");
+  const [seconds, setSeconds] = useState(10);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCount((count) => count + 1);
+    const interbalId = setInterval(() => {
+      setSeconds((current) => Math.max(current - 1, 0));
     }, 1000);
-  });
+
+    return () => {
+      // console.log("Cleaning up...");
+    };
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    (async function loadPost() {
+      try {
+        setStatus("loading");
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts?_limit=10",
+          { signal: controller.signal },
+        );
+        const data = await response.json();
+        setPosts(data);
+        setStatus("success");
+      } catch (error) {
+        if (error.name === "AbortError") {
+          console.log("Fetch aborted");
+        } else {
+          setStatus("error");
+        }
+      }
+    })();
+
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <>
       <h1>useEffect Hook</h1>
-      <p>Count: {count}</p>
+      
     </>
   );
 }
