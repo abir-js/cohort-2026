480310d1ace9466bb658993a4851f0ee7118bdce
Abir Bhattacharjee <abir.bhattacharjee2580@gmail.com>
2026-06-26 13:28:59 +0530

    01-variable and datatypes

---
+++ frontend/js/01-introduction/first.js
@@ -0,0 +1,11 @@
+var fname = "Abir";
+var lname = "Bhattacharjee";
+
+console.log("Value of fname is", fname);
+
+fname = "John";
+console.log("Value of fname is", fname);
+
+fname = 10;
+console.log("Value of fname is", fname);
+
